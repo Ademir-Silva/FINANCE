117dfa1daf8315f32ca2b1e62b326d3d9d4a8c00
Ademir-Silva <ademirfernandessilva622@gmail.com>
2021-03-03 22:40:16 -0300

    atualizado

---
+++ script.js
@@ -0,0 +1,9 @@
+/* const Log = {
+    sign_in(){
+        document.querySelector('button.sign-in')
+            .addEventListener('click' , event  =>{
+                    location.assign( "../pages/login.html")
+                    
+            } )
+    }
+} */
